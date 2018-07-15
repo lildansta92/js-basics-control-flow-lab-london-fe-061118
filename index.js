3aa249115904444c8c4ff71e297780e063eb67da
lildansta92 <danielgarcia1812@gmail.com>
2018-07-15 09:46:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+function myfunction(colour) {
+  if (colour == 'red') {
+    return 'red is nice!';
+  }
+  else {
+    return 'I want red!!!!!!';
+  }
+} 
+
+myFunction('iygffytff');
+
+
+
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+    
+  }
+  else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks'
+    
+  }
+}
